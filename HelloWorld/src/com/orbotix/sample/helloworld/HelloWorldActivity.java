63bc63cc80d85382e56e355c832a996751c9335c
Adam Williams <mail@adamwilliams.info>
2011-12-12 16:32:18 -0700

    Hello, World
    Added hello world app

--- HelloWorld/src/com/orbotix/sample/helloworld/HelloWorldActivity.java
+++ HelloWorld/src/com/orbotix/sample/helloworld/HelloWorldActivity.java
@@ -1,15 +1,92 @@
 package com.orbotix.sample.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import orbotix.robot.app.StartupActivity;
+import orbotix.robot.base.RGBLEDOutputCommand;
+import orbotix.robot.base.Robot;
+import orbotix.robot.base.RobotControl;
+import orbotix.robot.base.RobotProvider;
 
+/**
+ * Connects to an available Sphero robot, and then flashes its LED.
+ */
 public class HelloWorldActivity extends Activity
 {
+    /**
+     * ID for launching the StartupActivity for result to connect to the robot
+     */
+    private final static int STARTUP_ACTIVITY = 0;
+
+    /**
+     * The Sphero Robot
+     */
+    private Robot mRobot;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        //Launch the StartupActivity to connect to the robot
+        Intent i = new Intent(this, StartupActivity.class);
+        startActivityForResult(i, STARTUP_ACTIVITY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if(requestCode == STARTUP_ACTIVITY && resultCode == RESULT_OK){
+
+            //Get the connected Robot
+            final String robot_id = data.getStringExtra(StartupActivity.EXTRA_ROBOT_ID);
+            if(robot_id != null && !robot_id.equals("")){
+                mRobot = RobotProvider.getDefaultProvider().findRobot(robot_id);
+            }
+            
+            //Start blinking
+            blink(false);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mRobot = null;
+
+        //Disconnect Robot
+        RobotProvider.getDefaultProvider().disconnectControlledRobots();
+    }
+
+    /**
+     * Causes the robot to blink once every second.
+     * @param lit
+     */
+    private void blink(final boolean lit){
+        
+        if(mRobot != null){
+            
+            //If not lit, send command to show blue light, or else, send command to show no light
+            if(lit){
+                RGBLEDOutputCommand.sendCommand(mRobot, 0, 0, 0);
+            }else{
+                RGBLEDOutputCommand.sendCommand(mRobot, 0, 0, 255);
+            }
+            
+            //Send delayed message on a handler to run blink again
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    blink(!lit);
+                }
+            }, 1000);
+        }
     }
 }
