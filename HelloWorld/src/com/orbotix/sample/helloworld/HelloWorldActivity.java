db4be1c17a0de21d0187b5a9ce32d4a3232bb976
Adam Williams <mail@adamwilliams.info>
2011-12-12 14:45:11 -0700

    Created Hello World App

---
+++ HelloWorld/src/com/orbotix/sample/helloworld/HelloWorldActivity.java
@@ -0,0 +1,15 @@
+package com.orbotix.sample.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorldActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
