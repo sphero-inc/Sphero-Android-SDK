d6c3cf966915a653d760e69d1bfd84c788ac5fd2
Corey Earwood <corey@orbotix.com>
2016-07-29 15:25:29 -0600

    Remove local RobotLibrary.jar from DriveSample. Also had to fix some issues and remove RobotPicker (no longer necessary with dual stack discovery agent)

--- samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/MainActivity.java
+++ samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/MainActivity.java
@@ -25,13 +25,11 @@ import com.orbotix.joystick.api.JoystickEventListener;
 import com.orbotix.joystick.api.JoystickView;
 import com.orbotix.le.DiscoveryAgentLE;
 import com.orbotix.le.RobotLE;
-import com.orbotix.robotpicker.RobotPickerDialog;
 import com.orbotix.DualStackDiscoveryAgent;
 
 import java.util.List;
 
-public class MainActivity extends Activity implements //RobotPickerDialog.RobotPickerListener,
-                                                      DiscoveryAgentEventListener,
+public class MainActivity extends Activity implements DiscoveryAgentEventListener,
                                                       RobotChangedStateListener {
 
     private static final String TAG = "MainActivity";
@@ -40,11 +38,6 @@ public class MainActivity extends Activity implements //RobotPickerDialog.RobotP
      * Our current discovery agent that we will use to find robots of a certain protocol
      */
     private DiscoveryAgent _currentDiscoveryAgent;
-
-    /**
-     * The dialog that will allow the user to chose which type of robot to connect to
-     */
-    private RobotPickerDialog _robotPickerDialog;
 
     /**
      * The joystick that we will use to send roll commands to the robot
@@ -112,16 +105,6 @@ public class MainActivity extends Activity implements //RobotPickerDialog.RobotP
     @Override
     protected void onStart() {
         super.onStart();
-        // Create a robot picker dialog, this allows the user to select which robot they would like to connect to.
-        // We don't need to do this step if we know which robot we want to talk to, and don't need the user to
-        // decide that.
-//        if (_robotPickerDialog == null) {
-//            _robotPickerDialog = new RobotPickerDialog(this, this);
-//        }
-//        // Show the picker only if it's not showing. This keeps multiple calls to onStart from showing too many pickers.
-//        if (!_robotPickerDialog.isShowing()) {
-//            _robotPickerDialog.show();
-//        }
 
         if (_currentDiscoveryAgent == null) {
             _currentDiscoveryAgent = new DualStackDiscoveryAgent();
@@ -158,34 +141,6 @@ public class MainActivity extends Activity implements //RobotPickerDialog.RobotP
             }
         }
     }
-
-//    /**
-//     * Invoked when the user makes a selection on which robot they would like to use.
-//     * @param robotPicked The type of the robot that was selected
-//     */
-//    @Override
-//    public void onRobotPicked(RobotPickerDialog.RobotPicked robotPicked) {
-//        // Dismiss the robot picker so that the user doesn't keep clicking it and trying to start
-//        // discovery multiple times
-//        _robotPickerDialog.dismiss();
-//        switch (robotPicked) {
-//            // If the user picked a Sphero, you want to start the Bluetooth Classic discovery agent, as that is the
-//            // protocol that Sphero talks over. This will allow us to find a Sphero and connect to it.
-//            case Sphero:
-//                // To get to the classic discovery agent, you use DiscoveryAgentClassic.getInstance()
-//                _currentDiscoveryAgent = DiscoveryAgentClassic.getInstance();
-//                break;
-//            // If the user picked an Ollie, you want to start the Bluetooth LE discovery agent, as that is the protocol
-//            // that Ollie talks over. This will allow you to find an Ollie and connect to it.
-//            case Ollie:
-//                // To get to the LE discovery agent, you use DiscoveryAgentLE.getInstance()
-//                _currentDiscoveryAgent = DiscoveryAgentLE.getInstance();
-//                break;
-//        }
-//
-//        // Now that we have a discovery agent, we will start discovery on it using the method defined below
-//        startDiscovery();
-//    }
 
     /**
      * Invoked when the discovery agent finds a new available robot, or updates and already available robot
